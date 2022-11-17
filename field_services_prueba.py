02ff23bf195551e45c5422fc05b77e9d040bfea5
florver <105130181+florver@users.noreply.github.com>
2022-11-17 20:28:35 -0300

    Add files via upload

--- field_services_prueba.py
+++ field_services_prueba.py
@@ -156,17 +156,7 @@ def add_constraint_matrix(my_problem, data):
 
 
 
-      # Restricción "Una orden de trabajo debe tener asignada sus To trabajadores en un mismo turno para poder ser resuelta"
 
-      for n in range(len(data.ordenes)):
-        variables_restriccion = []
-        for j in range(data.cantidad_trabajadores):
-          for d in range(data.dias):
-            for t in range(data.turnos):
-              variables_restriccion.append('v'+'_'+str(j)+'_'+str(d)+'_'+str(t)+'_'+str(n))
-              values = [1]*len(variables_restriccion)
-              row = [variables_restriccion, values]
-              my_problem.linear_constraints.add(lin_expr=[row], senses=['G'], rhs=[data.ordenes[n].trabajadores_necesarios])
 
 
       # Restricción “Diferencia entre el trabajador con más órdenes asignadas y el trabajador con menos órdenes no puede ser mayor a 10"
