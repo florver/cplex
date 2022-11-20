73dd749594a4d4fa2ca8a11ed3fe83dbfe1c39b9
florver <105130181+florver@users.noreply.github.com>
2022-11-20 13:15:27 -0300

    Add files via upload

--- field_services_prueba.py
+++ field_services_prueba.py
@@ -169,6 +169,48 @@ def add_constraint_matrix(my_problem, data):
               my_problem.linear_constraints.add(lin_expr=[row], senses=['L'], rhs=[1.0])
 
 
+       # Restricción “Existen algunos pares de  ́ordenes de trabajo correlativas”  
+
+
+      for n in range(len(data.ordenes_correlativas)):
+        for d in range(data.dias):
+          variables_restriccion = []
+          for j in range(data.cantidad_trabajadores):
+            variables_restriccion.append('v'+'_'+str(j)+'_'+str(d)+'_'+str(4)+'_'+str(data.ordenes_correlativas[n][0]))
+            values = [1]*len(variables_restriccion) 
+            row = [variables_restriccion, values]
+            my_problem.linear_constraints.add(lin_expr=[row], senses=['L'], rhs=[0.0])
+
+
+      for n in range(len(data.ordenes_correlativas)):
+        for d in range(data.dias):
+          for t in range(data.turnos):
+            variables_restriccion_delta = []
+            variables_restriccion_delta.append('delta'+'_'+str(data.ordenes_correlativas[n][0])+'_'+str(d)+'_'+str(t))
+            variables_restriccion = []
+            for j in range(data.cantidad_trabajadores):
+              variables_restriccion.append('v'+'_'+str(j)+'_'+str(d)+'_'+str(t)+'_'+str(data.ordenes_correlativas[n][0]))
+              values = [1]*len(variables_restriccion) + [-1.0] * data.ordenes[data.ordenes_correlativas[n][0]].trabajadores_necesarios * len(variables_restriccion_delta)
+              row = [variables_restriccion + variables_restriccion_delta, values]
+              my_problem.linear_constraints.add(lin_expr=[row], senses=['L'], rhs=[0.0])
+
+
+      for n in range(len(data.ordenes_correlativas)):
+        for d in range(data.dias):
+          for t in range(data.turnos):
+            if t < data.turnos-1:
+              variables_restriccion_delta = []
+              variables_restriccion_delta.append('delta'+'_'+str(data.ordenes_correlativas[n][0])+'_'+str(d)+'_'+str(t))
+              variables_restriccion = []
+              for j in range(data.cantidad_trabajadores):
+                variables_restriccion.append('v'+'_'+str(j)+'_'+str(d)+'_'+str(t+1)+'_'+str(data.ordenes_correlativas[n][1]))
+                values = [1]*len(variables_restriccion) + [-1.0] * data.ordenes[data.ordenes_correlativas[n][1]].trabajadores_necesarios * len(variables_restriccion_delta)
+                row = [variables_restriccion + variables_restriccion_delta, values]
+                my_problem.linear_constraints.add(lin_expr=[row], senses=['G'], rhs=[0.0])
+            else:
+              continue
+
+
       # Restricción “Hay pares de órdenes de trabajo que no pueden ser satisfechas en turnos consecutivos de un trabajador”
 
       for n in range(len(data.ordenes_conflictivas)):
